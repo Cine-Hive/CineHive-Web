f39f4e00be0374de74bb1cbc0cc1fe6442fb4a80
jbg <kjbg4565388@gmail.com>
2025-04-17 02:38:49 +0900

    [Add]: 토큰 만료 시 자동으로 로그아웃 되도록 axios 인터셉터 적용

--- CineHive-Web/src/main.js
+++ CineHive-Web/src/main.js
@@ -1,12 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import router from './router/router';
 import store from './store/index.js';
+import axios from 'axios';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+axios.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response && error.response.status === 403) {
+        console.warn("⛔ 토큰 만료 감지, 자동 로그아웃 처리");
+
+        store.commit('SET_LOGOUT');
+        localStorage.removeItem('isLoggedIn');
+        localStorage.removeItem('user');
+        localStorage.removeItem('loginType');
+        localStorage.removeItem('token');
+
+        router.push('/auth');
+      }
+      return Promise.reject(error);
+    }
+);
+
 store.dispatch('initializeStore').then(() => {
   new Vue({
     store,
