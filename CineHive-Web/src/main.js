3f7870c72212dc445fc3c5545d030cb2cc40b815
jbg <kjbg4565388@gmail.com>
2025-04-17 05:07:10 +0900

    [Fix]: 토큰 만료 시 자동 로그아웃을 위해 JWT 디코딩 처리하여 만료 시간 확인하도록 로직 변경

--- CineHive-Web/src/main.js
+++ CineHive-Web/src/main.js
@@ -5,31 +5,43 @@ import store from './store/index.js';
 import axios from 'axios';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import { isTokenExpired } from './utils/auth';
 
 Vue.config.productionTip = false;
+
+const token = localStorage.getItem('token');
+if (token && isTokenExpired(token)) {
+    alert('🔒 토큰이 만료되었습니다. 자동 로그아웃 처리합니다.');
+    store.commit('SET_LOGOUT');
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('loginType');
+    router.push('/auth');
+}
 
 axios.interceptors.response.use(
     response => response,
     error => {
-      if (error.response && error.response.status === 403) {
-        console.warn("⛔ 토큰 만료 감지, 자동 로그아웃 처리");
+        if (error.response && error.response.status === 403) {
+            alert("⛔ 토큰 만료 감지, 자동 로그아웃 처리");
 
-        store.commit('SET_LOGOUT');
-        localStorage.removeItem('isLoggedIn');
-        localStorage.removeItem('user');
-        localStorage.removeItem('loginType');
-        localStorage.removeItem('token');
+            store.commit('SET_LOGOUT');
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            localStorage.removeItem('isLoggedIn');
+            localStorage.removeItem('loginType');
 
-        router.push('/auth');
-      }
-      return Promise.reject(error);
+            router.push('/auth');
+        }
+        return Promise.reject(error);
     }
 );
 
 store.dispatch('initializeStore').then(() => {
-  new Vue({
-    store,
-    router,
-    render: h => h(App)
-  }).$mount('#app');
+    new Vue({
+        store,
+        router,
+        render: h => h(App)
+    }).$mount('#app');
 });
