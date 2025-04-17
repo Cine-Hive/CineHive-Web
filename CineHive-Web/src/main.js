50c3829f53821dbb05d9908233be40d895c0bc43
jbg <kjbg4565388@gmail.com>
2025-04-17 11:11:12 +0900

    [Fix]: token 만료 값에 따른 자동 로그아웃에 대한 alert문 수정

--- CineHive-Web/src/main.js
+++ CineHive-Web/src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false;
 
 const token = localStorage.getItem('token');
 if (token && isTokenExpired(token)) {
-    alert('🔒 토큰이 만료되었습니다. 자동 로그아웃 처리합니다.');
+    alert('로그아웃 되었습니다. 다시 로그인해 주세요');
     store.commit('SET_LOGOUT');
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -24,7 +24,7 @@ axios.interceptors.response.use(
     response => response,
     error => {
         if (error.response && error.response.status === 403) {
-            alert("⛔ 토큰 만료 감지, 자동 로그아웃 처리");
+            console.log("⛔ 토큰 만료 감지, 자동 로그아웃 처리");
 
             store.commit('SET_LOGOUT');
             localStorage.removeItem('token');
