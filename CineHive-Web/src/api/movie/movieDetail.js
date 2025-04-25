c6263ec0be7deef2fdb0b995e05865c54bcb62c2
jbg <kjbg4565388@gmail.com>
2025-04-26 02:24:32 +0900

    [Ref]: 영화 상세정보 컨텍스트 경로 변경

--- CineHive-Web/src/api/movie/movieDetail.js
+++ CineHive-Web/src/api/movie/movieDetail.js
@@ -7,7 +7,7 @@ const API_BASE_URL = 'http://localhost:8081';
 // 영화 상세 정보 가져오기
 export const fetchMovieDetails = async (movieId) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/movies/${movieId}`);
+        const response = await axios.get(`${API_BASE_URL}/api/v1/movies/${movieId}`);
         return response.data;
     } catch (error) {
         console.error('영화 상세 정보를 가져오는 중 오류가 발생했습니다:', error);
@@ -18,7 +18,7 @@ export const fetchMovieDetails = async (movieId) => {
 // 유사 영화 목록 가져오기
 export const fetchSimilarMovies = async (movieId) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/movies/${movieId}/similar`);
+        const response = await axios.get(`${API_BASE_URL}/api/v1/movies/${movieId}/similar`);
         return response.data;
     } catch (error) {
         console.error('추천 영화를 가져오는 중 오류 발생:', error);
