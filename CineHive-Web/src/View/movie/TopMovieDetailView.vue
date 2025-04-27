60b6a1a7182a5730a40b182b9a4387a6a05ce917
jbg <kjbg4565388@gmail.com>
2025-04-27 11:55:42 +0900

    [Fix]: slice 적용하여 유사한 영화 정보 제공 최대 10개로 제한

--- CineHive-Web/src/View/movie/TopMovieDetailView.vue
+++ CineHive-Web/src/View/movie/TopMovieDetailView.vue
@@ -64,11 +64,10 @@
       </div>
     </div>
 
-    <!-- 추천 영화 섹션 추가 -->
     <div class="similar-movies-section" v-if="similarMovies.length > 0">
-      <h3 class="section-title">추천 영화</h3>
+      <h3 class="section-title">관련 추천 영화</h3>
       <div class="similar-movies-list">
-        <div v-for="similar in similarMovies" :key="similar.id" class="similar-movie-item" @click="goToMovieDetail(similar.id)">
+        <div v-for="similar in similarMovies.slice(0, 10)" :key="similar.id" class="similar-movie-item" @click="goToMovieDetail(similar.id)">
           <img v-if="similar.posterPath" :src="'https://image.tmdb.org/t/p/w200' + similar.posterPath" alt="추천 영화 포스터" />
           <p class="similar-movie-title">{{ similar.title }}</p>
         </div>
@@ -127,6 +126,8 @@ export default {
       if (videoData && videoData.key) {
         this.videoUrl = `https://www.youtube.com/embed/${videoData.key}`;
       }
+      // ⭐ 유사 영화 목록 가져오기
+      this.similarMovies = await fetchSimilarMovies(movieId);
 
       this.movie = await fetchMovieDetails(movieId);
       this.bookmarkCount = await this.fetchBookmarkCount(movieId);
