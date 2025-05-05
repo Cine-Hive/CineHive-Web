02bdae94128e167d1551f596f35af39550a8cdbd
jbg <kjbg4565388@gmail.com>
2025-05-05 19:13:39 +0900

    [Fix]: tv.js 관련 엔드포인트 변경

--- CineHive-Web/src/api/tv/tv.js
+++ CineHive-Web/src/api/tv/tv.js
@@ -1,53 +1,79 @@
-// HomeView.vue
+// api/tv.js
+
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8081/api/v1';
+const BASE_URL = 'http://localhost:8081/api/v1/media/tvs';
 
-
-// 인기 있는 TV 시리즈 가져오기
+/**
+ * 인기 있는 TV 시리즈 가져오기
+ */
 export const fetchPopularTvSeries = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/tv/popular`);
-        return response.data.results.slice(0, 18);
+        const response = await axios.get(BASE_URL, {
+            params: {
+                category: 'popular',
+                page: 1,
+                size: 18
+            }
+        });
+        return response.data.results;
     } catch (error) {
         console.error('인기 TV 시리즈 목록을 가져오는 중 오류 발생:', error);
         throw error;
     }
 };
 
-// 평점 높은 TV 시리즈 가져오기
+/**
+ * 평점 높은 TV 시리즈 가져오기
+ */
 export const fetchTopRatedTvSeries = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/tv/top-rated`);
-        return response.data.results.slice(0, 18);
+        const response = await axios.get(BASE_URL, {
+            params: {
+                category: 'top_rated',
+                page: 1,
+                size: 18
+            }
+        });
+        return response.data.results;
     } catch (error) {
         console.error('평점 높은 TV 시리즈 목록을 가져오는 중 오류 발생:', error);
         throw error;
     }
 };
 
-// 방영 중인 TV 시리즈 가져오기
+/**
+ * 방영 중인 TV 시리즈 가져오기
+ */
 export const fetchOnTheAirTvSeries = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/tv/on-the-air`);
-        return response.data.results.slice(0, 18);
+        const response = await axios.get(BASE_URL, {
+            params: {
+                category: 'on_the_air',
+                page: 1,
+                size: 18
+            }
+        });
+        return response.data.results;
     } catch (error) {
         console.error('방영 중인 TV 시리즈 목록을 가져오는 중 오류 발생:', error);
         throw error;
     }
 };
 
-// 오늘 방영하는 TV 시리즈 목록 가져오기
-export const fetchAiringTodayTvSeries = async (page = 1, sort = 'popularity.desc') => {
+/**
+ * 오늘 방영하는 TV 시리즈 목록 가져오기
+ */
+export const fetchAiringTodayTvSeries = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/tv/airing-today`, {
+        const response = await axios.get(BASE_URL, {
             params: {
-                page: page,
-                sort: sort
+                category: 'airing_today',
+                page: 1,
+                size: 18
             }
         });
-        console.log("res",response.data);
-        return response.data.results.slice(0, 18);  // 첫 18개 항목만 가져오기
+        return response.data.results;
     } catch (error) {
         console.error('오늘 방영하는 TV 시리즈 목록을 가져오는 중 오류 발생:', error);
         throw error;
